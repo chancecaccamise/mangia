4659913ff9fa526b44b78bdacb8bd6b55a2fd64e
chancecaccamise <ccaccamise@mail.niagara.edu>
2025-01-19 22:37:57 -0500

    added form and removed calendly and fixed error

--- app/meeting/contact-form.tsx
+++ app/meeting/contact-form.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
+import { useForm } from "@formspree/react";
 
 export function ExampleForm() {
   const [state, handleSubmit] = useForm("mkggekny");
